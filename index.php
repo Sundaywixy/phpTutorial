5b04c264e1cf31e7d7a7489479c4b2d451a01eac
Sundaywixy <sundayemmanuelng@aol.com>
2023-06-06 15:58:25 +0100

    maths functions

--- index.php
+++ index.php
@@ -7,29 +7,34 @@
     <title>Document</title>
 </head>
 <body>
-   <form action= "index.php" method= "post">
-    <label>Quantity: </label><br>
-    <input type="text" name= "quantity" > <br> <br>
-    <input type= "submit" value="total">
-</form> 
+<form action = "index.php" method = "post">
+<label>radius</label>
+<input type= "text" name= "radius"> <br>
+<input type= "submit" name= "calculate">
 
+</form> <br>
 
 </body>
 </html>
 
 
 
-
-
 <?php
-$item = "pizza";
-$price = "5.99";
-$quantity = $_POST["quantity"];
-$total = null;
+$radius = $_POST["radius"];
+$circumference = null;
+$area = null;
+$volume = null;
 
-$total = $quantity * $price;
+$circumference = 2 * pi() * $radius;
+$circumference = round($circumference, 2);
 
-echo "you have ordered {$quantity} X {$item}/s <br>"; 
-echo "your total is \${$total}";
+$volume = 4/3 * pi() * pow($radius, 2);
+$volume = round($volume, 2);
 
-?>
+
+$area = pi() * pow($radius, 2);
+$area = round($area, 2);
+
+echo "circumference = {$circumference}cm <br>";
+echo "area = {$area}cm^2 <br>";
+echo "volume = {$volume}cm^3 <br>";
